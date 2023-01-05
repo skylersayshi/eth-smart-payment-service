a9996c1013e6031e84c3c07cb9dc1bdb097a3555
giantnerd123 <payneskyler@yahoo.com>
2023-01-04 21:51:06 -0800

    cors update

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,22 @@ dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
-app.use(cors());
+// app.use(cors());
+var allowedOrigins = ['http://localhost:3000',
+                      'https://rodeopay.xyz/'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use('/users', userRoutes)
 app.use('/requests', requestRoutes)
